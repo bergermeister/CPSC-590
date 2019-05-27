f0fcc94647b1e8e7edc4d89752874c5d2fe45a9c
Edward Eisenberger <eeisenberger3@gmail.com>
2019-05-27 11:47:13 -0400

    Implemented Producer Consumer example

---
+++ Assignment02/ProducerConsumer/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace ProducerConsumer
+{
+   partial class Form1
+   {
+      /// <summary>
+      /// Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing && (components != null))
+         {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Windows Form Designer generated code
+
+      /// <summary>
+      /// Required method for Designer support - do not modify
+      /// the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent()
+      {
+         this.voBtnConsumerStart = new System.Windows.Forms.Button();
+         this.voBtnProducerStart = new System.Windows.Forms.Button();
+         this.voBtnConsumerMR = new System.Windows.Forms.Button();
+         this.voBtnProducerMR = new System.Windows.Forms.Button();
+         this.voStatus = new System.Windows.Forms.StatusStrip();
+         this.voLabel = new System.Windows.Forms.Label();
+         this.SuspendLayout();
+         // 
+         // voBtnConsumerStart
+         // 
+         this.voBtnConsumerStart.Location = new System.Drawing.Point(12, 12);
+         this.voBtnConsumerStart.Name = "voBtnConsumerStart";
+         this.voBtnConsumerStart.Size = new System.Drawing.Size(146, 36);
+         this.voBtnConsumerStart.TabIndex = 0;
+         this.voBtnConsumerStart.Text = "Start Consumer";
+         this.voBtnConsumerStart.UseVisualStyleBackColor = true;
+         this.voBtnConsumerStart.Click += new System.EventHandler(this.voBtnConsumerStart_Click);
+         // 
+         // voBtnProducerStart
+         // 
+         this.voBtnProducerStart.Location = new System.Drawing.Point(218, 12);
+         this.voBtnProducerStart.Name = "voBtnProducerStart";
+         this.voBtnProducerStart.Size = new System.Drawing.Size(146, 36);
+         this.voBtnProducerStart.TabIndex = 1;
+         this.voBtnProducerStart.Text = "Start Producer";
+         this.voBtnProducerStart.UseVisualStyleBackColor = true;
+         this.voBtnProducerStart.Click += new System.EventHandler(this.voBtnProducerStart_Click);
+         // 
+         // voBtnConsumerMR
+         // 
+         this.voBtnConsumerMR.Location = new System.Drawing.Point(12, 80);
+         this.voBtnConsumerMR.Name = "voBtnConsumerMR";
+         this.voBtnConsumerMR.Size = new System.Drawing.Size(146, 36);
+         this.voBtnConsumerMR.TabIndex = 2;
+         this.voBtnConsumerMR.Text = "Consumer MR";
+         this.voBtnConsumerMR.UseVisualStyleBackColor = true;
+         this.voBtnConsumerMR.Click += new System.EventHandler(this.voBtnConsumerMR_Click);
+         // 
+         // voBtnProducerMR
+         // 
+         this.voBtnProducerMR.Location = new System.Drawing.Point(218, 80);
+         this.voBtnProducerMR.Name = "voBtnProducerMR";
+         this.voBtnProducerMR.Size = new System.Drawing.Size(146, 36);
+         this.voBtnProducerMR.TabIndex = 3;
+         this.voBtnProducerMR.Text = "Producer MR";
+         this.voBtnProducerMR.UseVisualStyleBackColor = true;
+         this.voBtnProducerMR.Click += new System.EventHandler(this.voBtnProducerMR_Click);
+         // 
+         // voStatus
+         // 
+         this.voStatus.ImageScalingSize = new System.Drawing.Size(24, 24);
+         this.voStatus.Location = new System.Drawing.Point(0, 428);
+         this.voStatus.Name = "voStatus";
+         this.voStatus.Size = new System.Drawing.Size(800, 22);
+         this.voStatus.TabIndex = 4;
+         this.voStatus.Text = "statusStrip1";
+         // 
+         // voLabel
+         // 
+         this.voLabel.AutoSize = true;
+         this.voLabel.Location = new System.Drawing.Point(12, 160);
+         this.voLabel.Name = "voLabel";
+         this.voLabel.Size = new System.Drawing.Size(0, 20);
+         this.voLabel.TabIndex = 5;
+         // 
+         // Form1
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.ClientSize = new System.Drawing.Size(800, 450);
+         this.Controls.Add(this.voLabel);
+         this.Controls.Add(this.voStatus);
+         this.Controls.Add(this.voBtnProducerMR);
+         this.Controls.Add(this.voBtnConsumerMR);
+         this.Controls.Add(this.voBtnProducerStart);
+         this.Controls.Add(this.voBtnConsumerStart);
+         this.Name = "Form1";
+         this.Text = "Form1";
+         this.ResumeLayout(false);
+         this.PerformLayout();
+
+      }
+
+      #endregion
+
+      private System.Windows.Forms.Button voBtnConsumerStart;
+      private System.Windows.Forms.Button voBtnProducerStart;
+      private System.Windows.Forms.Button voBtnConsumerMR;
+      private System.Windows.Forms.Button voBtnProducerMR;
+      private System.Windows.Forms.StatusStrip voStatus;
+      private System.Windows.Forms.Label voLabel;
+   }
+}
+
---
+++ Assignment02/ProducerConsumer/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace ProducerConsumer
+{
+   public partial class Form1 : Form
+   {
+      private delegate void ToDisplay( string aoMsg );
+
+      private Thread           voConsumer;
+      private Thread           voProducer;
+      private Thread           voConsumerMR;
+      private Thread           voProducerMR;
+      private object           voLock = new object( );
+      private bool             vbDataReady = false;
+      private int              viData = 0;
+      private ToDisplay        voDel = null;
+      private ManualResetEvent voMR = new ManualResetEvent( false );
+
+      public Form1( )
+      {
+         InitializeComponent();
+
+         this.voDel = new ToDisplay( mDisplayStatus ); 
+      }
+
+      private void voBtnConsumerStart_Click(object sender, EventArgs e)
+      {
+         this.voConsumer = new Thread( new ThreadStart( this.mConsumer ) );
+         this.voConsumer.Start( );
+      }
+
+      private void voBtnProducerStart_Click(object sender, EventArgs e)
+      {
+         this.voProducer = new Thread( new ThreadStart( this.mProducer ) );
+         this.voProducer.Start( );
+      }
+
+      private void voBtnConsumerMR_Click(object sender, EventArgs e)
+      {
+         this.voConsumerMR = new Thread( new ThreadStart( this.mConsumerMR ) );
+         this.voConsumerMR.Start( );
+      }
+
+      private void voBtnProducerMR_Click(object sender, EventArgs e)
+      {
+         this.voProducerMR = new Thread( new ThreadStart( this.mProducerMR ) );
+         this.voProducerMR.Start( );
+      }
+
+      private void mDisplayStatus( string aoMsg )
+      {
+         this.voLabel.Text = aoMsg;
+      }
+
+      private void mConsumer( )
+      {
+         for( int kiI = 0; kiI < 2; kiI++ )
+         {
+            this.voStatus.Invoke( this.voDel, new string[ ]{ "Consumer thread started.." } );
+            if( this.vbDataReady == false )
+            {
+               lock( this.voLock )
+               {
+                  Monitor.Wait( this.voLock );
+               }
+               // Monitor.Wait and Monitor.Pulse are required to be inside the same lock
+               // they are testing
+            }
+            Thread.Sleep( 1000 );
+            this.voStatus.Invoke( this.voDel, new string[ ]{ "Starting to consume data.." } );
+            lock( this.voLock )
+            {
+               this.viData -= 2000;
+               this.vbDataReady = false;
+            }
+            Thread.Sleep( 4000 ); // Pretend it takes 4 seconds to consume data
+            this.voStatus.Invoke( this.voDel, new string[ ]{ "Data consumed: data = " + this.viData.ToString( ) } );
+         }
+      }
+
+      private void mProducer( )
+      {
+         this.voStatus.Invoke( this.voDel, new string[ ]{ "Producer started.." } );
+         Thread.Sleep( 9000 );   // 9 secs to produce data
+         lock( this.voLock )
+         {
+            this.viData = 2375;
+            this.vbDataReady = true;
+         }
+         lock( this.voLock )
+         {
+            Monitor.Pulse( this.voLock ); // Signal waiting thread
+         }
+      }
+
+      private void mConsumerMR( )
+      {
+         // Use ManualResetEvent for synchronization
+         for( int kiI = 0; kiI < 2; kiI++ )
+         {
+            this.voStatus.Invoke( this.voDel, new string[ ]{ "Consumer thread started.." } );
+            if( this.vbDataReady == false )
+            {
+               this.voMR.WaitOne( );   // Wait for producer
+               this.voMR.Reset( );     // Close the gate
+            }
+            Thread.Sleep( 1000 );
+            this.voStatus.Invoke( this.voDel, new string[ ]{ "Starting to consume data.." } );
+            lock( this.voLock )
+            {
+               this.viData -= 2000;
+               this.vbDataReady = false;
+            }
+            Thread.Sleep( 4000 ); // Pretend it takes 4 seconds to consume data
+            this.voStatus.Invoke( this.voDel, new string[ ]{ "Data consumed: data = " + this.viData.ToString( ) } );
+         }
+      }
+
+      private void mProducerMR( )
+      {
+         this.voStatus.Invoke( this.voDel, new string[ ]{ "Producer started.." } );
+         Thread.Sleep( 9000 );   // 9 seconds to produce data
+         lock( this.voLock )
+         {
+            this.viData = 2375;
+            this.vbDataReady = true;
+         }
+         this.voMR.Set( ); // Send signal to waiting thread;
+      }
+   }
+}
