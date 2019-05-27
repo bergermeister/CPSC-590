a4de0f5d0cb8b42b54f3ea0cf9255c2f62df48b5
Edward Eisenberger <eeisenberger3@gmail.com>
2019-05-27 13:11:58 -0400

    Implemented MutexTest example

---
+++ Assignment02/MutexTest/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace MutexTest
+{
+   partial class Form1
+   {
+      /// <summary>
+      /// Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing && (components != null))
+         {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Windows Form Designer generated code
+
+      /// <summary>
+      /// Required method for Designer support - do not modify
+      /// the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent()
+      {
+         this.voBtnTest = new System.Windows.Forms.Button();
+         this.voSb1 = new System.Windows.Forms.StatusStrip();
+         this.voLbl = new System.Windows.Forms.Label();
+         this.SuspendLayout();
+         // 
+         // voBtnTest
+         // 
+         this.voBtnTest.Location = new System.Drawing.Point(12, 12);
+         this.voBtnTest.Name = "voBtnTest";
+         this.voBtnTest.Size = new System.Drawing.Size(160, 31);
+         this.voBtnTest.TabIndex = 0;
+         this.voBtnTest.Text = "Mutex Test";
+         this.voBtnTest.UseVisualStyleBackColor = true;
+         this.voBtnTest.Click += new System.EventHandler(this.voBtnTest_Click);
+         // 
+         // voSb1
+         // 
+         this.voSb1.ImageScalingSize = new System.Drawing.Size(24, 24);
+         this.voSb1.Location = new System.Drawing.Point(0, 422);
+         this.voSb1.Name = "voSb1";
+         this.voSb1.Size = new System.Drawing.Size(800, 28);
+         this.voSb1.TabIndex = 1;
+         // 
+         // voLbl
+         // 
+         this.voLbl.AutoSize = true;
+         this.voLbl.Location = new System.Drawing.Point(178, 17);
+         this.voLbl.Name = "voLbl";
+         this.voLbl.Size = new System.Drawing.Size(0, 20);
+         this.voLbl.TabIndex = 2;
+         // 
+         // Form1
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.ClientSize = new System.Drawing.Size(800, 450);
+         this.Controls.Add(this.voLbl);
+         this.Controls.Add(this.voSb1);
+         this.Controls.Add(this.voBtnTest);
+         this.Name = "Form1";
+         this.Text = "Form1";
+         this.ResumeLayout(false);
+         this.PerformLayout();
+
+      }
+
+      #endregion
+
+      private System.Windows.Forms.Button voBtnTest;
+      private System.Windows.Forms.StatusStrip voSb1;
+      private System.Windows.Forms.Label voLbl;
+   }
+}
+
---
+++ Assignment02/MutexTest/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace MutexTest
+{
+   public partial class Form1 : Form
+   {
+      public Form1()
+      {
+         InitializeComponent();
+      }
+
+      private void voBtnTest_Click( object aoSender, EventArgs aoArgs )
+      {
+         bool  kbReqCreation = true; // To request creation of a mutex
+         bool  kbAlreadyExists;      // If a mutex already exists
+         int   kiId = System.AppDomain.GetCurrentThreadId( );
+         Mutex koM = new Mutex( kbReqCreation, "MyMutex", out kbAlreadyExists );
+
+         // Check to see if the mutex is owned by this thread
+         if( !( kbReqCreation && kbAlreadyExists ) )
+         {
+            this.voLbl.Text = "Mutex already exists, waiting on its release..." + kiId.ToString( );
+            koM.WaitOne( );
+         }
+
+         this.voLbl.Text = "Starting to use the resource, .. mutex owner" + kiId.ToString( );
+
+         // If this is the owner thread, you can use the shared resource
+         Thread.Sleep( 10000 );
+         // Done usuing this resource
+         this.voLbl.Text = "Done using mutex, releasing resource ..." + kiId.ToString( );
+         koM.ReleaseMutex( );
+      }
+   }
+}
