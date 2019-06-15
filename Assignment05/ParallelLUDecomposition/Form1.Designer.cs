cf5903c95431f70d3d2093b4aa9d146cd4df2689
Eisenberger, Edward <edward.eisenberger@live.com>
2019-06-15 11:23:42 -0400

    Assignment05.1 Implementation

---
+++ Assignment05/ParallelLUDecomposition/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace ParallelLUDecomposition
+{
+   partial class Form1
+   {
+      /// <summary>
+      /// Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose( bool disposing )
+      {
+         if( disposing && ( components != null ) )
+         {
+            components.Dispose( );
+         }
+         base.Dispose( disposing );
+      }
+
+      #region Windows Form Designer generated code
+
+      /// <summary>
+      /// Required method for Designer support - do not modify
+      /// the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent( )
+      {
+         this.voBtnSequential = new System.Windows.Forms.Button();
+         this.voBtnParallel = new System.Windows.Forms.Button();
+         this.voTbSize = new System.Windows.Forms.TextBox();
+         this.voLblSize = new System.Windows.Forms.Label();
+         this.SuspendLayout();
+         // 
+         // voBtnSequential
+         // 
+         this.voBtnSequential.Location = new System.Drawing.Point(12, 38);
+         this.voBtnSequential.Name = "voBtnSequential";
+         this.voBtnSequential.Size = new System.Drawing.Size(177, 23);
+         this.voBtnSequential.TabIndex = 0;
+         this.voBtnSequential.Text = "Sequential LU Decomposition";
+         this.voBtnSequential.UseVisualStyleBackColor = true;
+         this.voBtnSequential.Click += new System.EventHandler(this.voBtnSequential_Click);
+         // 
+         // voBtnParallel
+         // 
+         this.voBtnParallel.Location = new System.Drawing.Point(12, 67);
+         this.voBtnParallel.Name = "voBtnParallel";
+         this.voBtnParallel.Size = new System.Drawing.Size(177, 23);
+         this.voBtnParallel.TabIndex = 1;
+         this.voBtnParallel.Text = "Parallel LU Decomposition";
+         this.voBtnParallel.UseVisualStyleBackColor = true;
+         // 
+         // voTbSize
+         // 
+         this.voTbSize.Location = new System.Drawing.Point(78, 12);
+         this.voTbSize.Name = "voTbSize";
+         this.voTbSize.Size = new System.Drawing.Size(111, 20);
+         this.voTbSize.TabIndex = 2;
+         // 
+         // voLblSize
+         // 
+         this.voLblSize.AutoSize = true;
+         this.voLblSize.Location = new System.Drawing.Point(12, 15);
+         this.voLblSize.Name = "voLblSize";
+         this.voLblSize.Size = new System.Drawing.Size(60, 13);
+         this.voLblSize.TabIndex = 3;
+         this.voLblSize.Text = "Block Size:";
+         // 
+         // Form1
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.ClientSize = new System.Drawing.Size(800, 450);
+         this.Controls.Add(this.voLblSize);
+         this.Controls.Add(this.voTbSize);
+         this.Controls.Add(this.voBtnParallel);
+         this.Controls.Add(this.voBtnSequential);
+         this.Name = "Form1";
+         this.Text = "Form1";
+         this.ResumeLayout(false);
+         this.PerformLayout();
+
+      }
+
+      #endregion
+
+      private System.Windows.Forms.Button voBtnSequential;
+      private System.Windows.Forms.Button voBtnParallel;
+      private System.Windows.Forms.TextBox voTbSize;
+      private System.Windows.Forms.Label voLblSize;
+   }
+}
+
---
+++ Assignment05/ParallelLUDecomposition/Matrix.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelLUDecomposition
+{
+   class Matrix
+   {
+      private int         viRows;
+      private int         viCols;
+      private double[ , ] vdData;
+      
+      public Matrix( int aiRows, int aiColumns )
+      {
+         this.viRows = aiRows;
+         this.viCols = aiColumns;
+         this.vdData = new double[ this.viRows, this.viCols ];
+      }
+
+      public int ViRows
+      {
+         get{ return( this.viRows ); }
+      }
+
+      public int ViColumns
+      {
+         get{ return( this.viCols ); }
+      }
+
+      public double this[ int aiI, int aiJ ]
+      {
+         get{ return( this.vdData[ aiI, aiJ ] ); }
+         set{ this.vdData[ aiI, aiJ ] = value; }
+      }
+
+      public void MLUDecompose( double[ , ] adL, double[ , ] adU, ref double adError )
+      {
+         if( this.viRows != this.viCols )
+         {
+            throw new Exception( "Row and Column dimensions are not the same." );
+         }
+
+         /// -# Copy data into a matrix
+         double[ , ] kdA; // = new double[ this.viRows, this.viCols ];
+         double[ , ] kdRes = new double[ this.viRows, this.viRows ];
+
+         kdA = ( double[ , ] )this.vdData.Clone( );
+
+         for( int kiK = 0; kiK < this.viRows; kiK++ )
+         {
+            adU[ kiK, kiK ] = this.vdData[ kiK, kiK ];
+            for( int kiJ = kiK + 1; kiJ < this.viRows; kiJ++ )
+            {
+               adU[ kiK, kiJ ] = this.vdData[ kiK, kiJ ];
+            }
+            for( int kiI = kiK; kiI < this.viRows; kiI++ )
+            {
+               if( kiI == kiK )
+               {
+                  adL[ kiI, kiK ] = 1;
+               }
+               else
+               {
+                  adL[ kiI, kiK ] = this.vdData[ kiI, kiK ] / adU[ kiK, kiK ];
+               }
+            }
+            for( int kiI = kiK + 1; kiI < this.viRows; kiI++ )
+            {
+               for( int kiJ = kiK + 1; kiJ < this.viRows; kiJ++ )
+               {
+                  this.vdData[ kiI, kiJ ] = this.vdData[ kiI, kiJ ] - adL[ kiI, kiK ] * adU[ kiK, kiJ ];
+               }
+            }    
+         }
+
+         // verify if LU decomp is correct
+         
+         for( int kiI = 0; kiI < this.viRows; kiI++ )
+         {
+            for( int kiJ = 0; kiJ < this.viRows; kiJ++ )
+            {
+               for( int kiK = 0; kiK < this.viRows; kiK++ )
+               {
+                  kdRes[ kiI, kiJ ] += adL[ kiI, kiK ] * adU[ kiK, kiJ ];
+               }
+            }
+         }
+         adError = 0;
+         for( int kiI = 0; kiI < this.viRows; kiI++ )
+         {
+            for( int kiJ = 0; kiJ < this.viRows; kiJ++ )
+            {
+               adError += Math.Abs( kdRes[ kiI, kiJ ] - kdA[ kiI, kiJ ] );
+            }
+         }
+      }
+
+      public override string ToString()
+      {
+         string koOut = "";
+
+         for( int kiI = 0; kiI < this.viRows; kiI++ )
+         {
+            for( int kiJ = 0; kiJ < this.viCols; kiJ++ )
+            {
+               koOut += this.vdData[ kiI, kiJ ].ToString() + " \t";
+            }
+            koOut += Environment.NewLine;
+         }
+
+         return( koOut );
+      }
+
+      public static Matrix operator*( Matrix aoA, Matrix aoB )
+      {
+         Matrix koRes = new Matrix( aoA.ViRows, aoB.ViColumns );
+         Parallel.For( 0, aoA.ViRows, ( kiI ) =>
+         {
+            for( int kiK = 0; kiK < aoA.ViColumns; kiK++ )
+            {
+               for( int kiJ = 0; kiJ < aoB.ViColumns; kiJ++ )
+               {
+                  koRes.vdData[ kiI, kiJ ] += aoA.vdData[ kiI, kiK ] * aoB.vdData[ kiK, kiJ ];
+               }
+            }
+         } );
+
+         return( koRes );
+      }
+   }
+}
