153cfb5fbef0a0f11f97f5427e553e3a97c2cef1
Edward Eisenberger <eeisenberger3@gmail.com>
2019-06-25 18:49:41 -0400

    Added solution for GMM

--- Assignment07/GMM/Form1.Designer.cs
+++ Assignment07/GMM/Form1.Designer.cs
@@ -28,12 +28,70 @@
       /// </summary>
       private void InitializeComponent( )
       {
-         this.components = new System.ComponentModel.Container( );
+         this.voBtnTestGMM = new System.Windows.Forms.Button();
+         this.voBtnTestClass = new System.Windows.Forms.Button();
+         this.label1 = new System.Windows.Forms.Label();
+         this.textBox1 = new System.Windows.Forms.TextBox();
+         this.SuspendLayout();
+         // 
+         // voBtnTestGMM
+         // 
+         this.voBtnTestGMM.Location = new System.Drawing.Point(12, 12);
+         this.voBtnTestGMM.Name = "voBtnTestGMM";
+         this.voBtnTestGMM.Size = new System.Drawing.Size(75, 23);
+         this.voBtnTestGMM.TabIndex = 0;
+         this.voBtnTestGMM.Text = "Test GMM";
+         this.voBtnTestGMM.UseVisualStyleBackColor = true;
+         this.voBtnTestGMM.Click += new System.EventHandler(this.voBtnTestGMM_Click);
+         // 
+         // voBtnTestClass
+         // 
+         this.voBtnTestClass.Location = new System.Drawing.Point(12, 41);
+         this.voBtnTestClass.Name = "voBtnTestClass";
+         this.voBtnTestClass.Size = new System.Drawing.Size(75, 23);
+         this.voBtnTestClass.TabIndex = 1;
+         this.voBtnTestClass.Text = "Test Class";
+         this.voBtnTestClass.UseVisualStyleBackColor = true;
+         this.voBtnTestClass.Click += new System.EventHandler(this.voBtnTestClass_Click);
+         // 
+         // label1
+         // 
+         this.label1.AutoSize = true;
+         this.label1.Location = new System.Drawing.Point(110, 17);
+         this.label1.Name = "label1";
+         this.label1.Size = new System.Drawing.Size(72, 13);
+         this.label1.TabIndex = 2;
+         this.label1.Text = "Enter Number";
+         // 
+         // textBox1
+         // 
+         this.textBox1.Location = new System.Drawing.Point(113, 33);
+         this.textBox1.Name = "textBox1";
+         this.textBox1.Size = new System.Drawing.Size(100, 20);
+         this.textBox1.TabIndex = 3;
+         // 
+         // Form1
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
          this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.ClientSize = new System.Drawing.Size(284, 261);
+         this.Controls.Add(this.textBox1);
+         this.Controls.Add(this.label1);
+         this.Controls.Add(this.voBtnTestClass);
+         this.Controls.Add(this.voBtnTestGMM);
+         this.Name = "Form1";
          this.Text = "Form1";
+         this.ResumeLayout(false);
+         this.PerformLayout();
+
       }
 
       #endregion
+
+      private System.Windows.Forms.Button voBtnTestGMM;
+      private System.Windows.Forms.Button voBtnTestClass;
+      private System.Windows.Forms.Label label1;
+      private System.Windows.Forms.TextBox textBox1;
    }
 }
 
--- Assignment07/GMM/Form1.cs
+++ Assignment07/GMM/Form1.cs
@@ -12,9 +12,74 @@ namespace GMM
 {
    public partial class Form1 : Form
    {
+      // reference for equations involved in Gaussian Mixture Model using Expectation Maximization         
+      // https://brilliant.org/wiki/gaussian-mixture-model/ 
+
+      private int viK = 2; // Number of clusters
+      private int viDataSize = 20;
+      private double[ ] vdMu;
+      private Random  voRand = new Random( );
+      double[ ]   vdSigma = null; // Standard deviation for cluter k
+      double[ , ] vdPDF   = null; // Calculated pdf for each data point based on mean and var for cluster k
+      double[ , ] vdGamma = null; // probability matrix for each data point (i.e. probability that a data point belongs to cluster)
+      double[ ]   vdPHI   = null; // Prior probabilities for each cluster
+
       public Form1( )
       {
          InitializeComponent( );
+      }
+
+      private void voBtnTestGMM_Click( object sender, EventArgs e )
+      {
+
+      }
+
+      private void voBtnTestClass_Click( object sender, EventArgs e )
+      {
+         double[ ] kdX1 = new double[ 10 ];
+         double[ ] kdX2 = new double[ 10 ];
+         double[ ] kdX;
+
+         this.vdMu    = new double[ this.viK ];
+         this.vdSigma = new double[ this.viK ];
+         this.vdPHI   = new double[ this.viK ];
+         this.vdGamma = new double[ this.viDataSize, this.viK ];
+         this.vdPDF   = new double[ this.viDataSize, this.viK ];
+
+         this.vdMu   [ 0 ] = 10; this.vdMu   [ 1 ] = 15;
+         this.vdSigma[ 0 ] = 1;  this.vdSigma[ 1 ] = 3;
+
+         for( int i = 0; i < 10; i++ )
+         {
+            kdX1[ i ] = ( this.voRand.NextDouble( ) * this.vdSigma[ 0 ] ) + this.vdMu[ 0 ];
+            kdX2[ i ] = ( this.voRand.NextDouble( ) * this.vdSigma[ 1 ] ) + this.vdMu[ 1 ];
+         }
+
+         kdX = kdX1.Concat( kdX2 ).ToArray< double >( );
+
+         /// -# Step: Initialization - Randomly select k data points to act as means
+         List< int > koRList = new List< int >( );
+         for( int i = 0; i < this.viK; i++ )
+         {
+            int kiRPos = this.voRand.Next( kdX.Length );
+            if( koRList.Contains( kiRPos ) )
+            {
+               kiRPos = this.voRand.Next( kdX.Length );
+            }
+            this.vdMu[ i ] = kdX[ kiRPos ];
+         }
+
+         double kdVarianceOfData = ComputeVariance( kdX );
+         for( int i = 0; i < this.viK; i++ )
+         {
+            this.vdSigma[ i ] = Math.Sqrt( kdVarianceOfData );
+         }
+
+         // Set prior probabilities of each cluster to be uniform.
+         for( int i = 0; i < this.viK; i++ )
+         {
+            this.vdPHI[ i ] = 1.0 / this.viK;
+         }
       }
    }
 }
