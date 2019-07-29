884ede71a670a40867532de0951894c554589222
Edward Eisenberger <eeisenberger3@gmail.com>
2019-07-29 19:38:56 -0400

    Implemented BBO algorithm

--- Assignment08/GAWebHost/Distributor.svc.cs
+++ Assignment08/GAWebHost/Distributor.svc.cs
@@ -3,14 +3,15 @@
    using System;
    using System.Collections.Generic;
    using System.ServiceModel;
-   using System.Threading.Tasks;
    using GAWebLib;
 
    public class Distributor : IDistributor, Servant.IWorkerCallback, Servant2.IWorkerCallback
    {
       private double[ ][ ]                 vdDistance;
       private int                          viNumWorkers;
-      private List< Pair< Worker, bool > > voWorkers;
+      private List< Habitat > voEcosystem;
+
+      private static Random voRand = new Random( DateTime.Now.Millisecond );
       
       public void MExecute( double[ ][ ] adDistMat, int aiNumWorkers )
       {
@@ -42,7 +43,7 @@
             koBest = new Member[ this.viNumWorkers ];
 
             /// -# Create a list of workers and updated flags
-            this.voWorkers = new List< Pair< Worker, bool > >( this.viNumWorkers );
+            this.voEcosystem = new List< Habitat >( this.viNumWorkers );
 
             /// -# Update the Constants
             Constants.NumCities = adDistMat.Length;
@@ -50,8 +51,8 @@
             /// -# Initialize Workers
             for( int kiI = 0; kiI < this.viNumWorkers; kiI++ )
             {
-               this.voWorkers.Add( new Pair< Worker, bool >( new Worker( ), false ) );
-               this.voWorkers[ kiI ].VoItem1.MInitialize( Constants.PopSize, 
+               this.voEcosystem.Add( new Habitat( new Worker( ) ) );
+               this.voEcosystem[ kiI ].VoWorker.MInitialize( Constants.PopSize, 
                                                           Constants.NumCities, 
                                                           Constants.MutationRate, 
                                                           Constants.CrossoverRate, 
@@ -64,27 +65,28 @@
             for( int kiIter = 0; kiIter < Constants.NumIterations; kiIter += Constants.ExchangeAfterIterations )
             {
                /// -# Launch each worker to run ExchangeAfterIterations number of iterations (default:500)
-               koClient1.MRun( this.voWorkers[ 0 ].VoItem1, this.vdDistance, Constants.ExchangeAfterIterations );
-               koClient2.MRun( this.voWorkers[ 1 ].VoItem1, this.vdDistance, Constants.ExchangeAfterIterations );
+               koClient1.MRun( this.voEcosystem[ 0 ].VoWorker, this.vdDistance, Constants.ExchangeAfterIterations );
+               koClient2.MRun( this.voEcosystem[ 1 ].VoWorker, this.vdDistance, Constants.ExchangeAfterIterations );
 
                /// -# Wait for the workers to complete
                for( int kiI = 0; kiI < this.viNumWorkers; kiI++ )
                {
-                  while( !this.voWorkers[ kiI ].VoItem2 );
-                  this.voWorkers[ kiI ].VoItem2 = false;
-                  koBest[ kiI ] = this.voWorkers[ kiI ].VoItem1.VoBest;
+                  while( !this.voEcosystem[ kiI ].VbComplete );
+                  this.voEcosystem[ kiI ].VbComplete = false;
+                  koBest[ kiI ] = this.voEcosystem[ kiI ].VoWorker.VoBest;
                }
 
                /// -# Send Update to client
                koChannel.MOnUpdate( koBest );
                Console.WriteLine( "{0} - Distributor: UPDATE", DateTime.Now );
 
-               this.mExchangeData( );       
+               this.mExchangeBBO( );
+               //this.mExchangeData( );       
                for( int kiI = 0; kiI < this.viNumWorkers; kiI++ )
                {
-                  this.voWorkers[ kiI ].VoItem1.MMutatePopulation( );
-                  this.voWorkers[ kiI ].VoItem1.MEvaluatePopulation( );
-                  Array.Sort( this.voWorkers[ kiI ].VoItem1.VoPopulation );
+                  this.voEcosystem[ kiI ].VoWorker.MMutatePopulation( );
+                  this.voEcosystem[ kiI ].VoWorker.MEvaluatePopulation( );
+                  Array.Sort( this.voEcosystem[ kiI ].VoWorker.VoPopulation );
                }
             }
 
@@ -101,25 +103,24 @@
       public void MOnComplete( Worker aoWorker )
       {
          Console.WriteLine( "{0} - Distributor: WORKER {1} COMPLETE", DateTime.Now, aoWorker.ViWorkerNum );
-         this.voWorkers[ aoWorker.ViWorkerNum ].VoItem1 = ( Worker )aoWorker.Clone( );
-         this.voWorkers[ aoWorker.ViWorkerNum ].VoItem2 = true;
+         this.voEcosystem[ aoWorker.ViWorkerNum ].VoWorker = ( Worker )aoWorker.Clone( );
+         this.voEcosystem[ aoWorker.ViWorkerNum ].VbComplete = true;
       }
 
       private void mExchangeData( )
       {
          int    kiI, kiW1, kiW2, kiM1, kiM2;
-         Random koRand = new Random( DateTime.Now.Millisecond );
          Member koTemp;
 
          // Exchange data for each worker
          for( kiI = 0; kiI < this.viNumWorkers; kiI++ )
          {
-            kiW1 = ( int )( koRand.NextDouble( ) * this.viNumWorkers );
-            kiW2 = ( int )( koRand.NextDouble( ) * this.viNumWorkers );
+            kiW1 = ( int )( voRand.NextDouble( ) * this.viNumWorkers );
+            kiW2 = ( int )( voRand.NextDouble( ) * this.viNumWorkers );
 
             if( kiW1 == kiW2 )
             {
-               kiW2 = ( int )( koRand.NextDouble( ) * this.viNumWorkers );
+               kiW2 = ( int )( voRand.NextDouble( ) * this.viNumWorkers );
             }
              
             kiM1 = 0; 
@@ -127,11 +128,55 @@
             
             for( int kiJ = 0; kiJ < Constants.NumMembersToExchange; kiJ++ )  // exchange 3 values
             {
-               kiM1 = ( int )( koRand.NextDouble( ) * Constants.PopSize );
-               kiM2 = ( int )( koRand.NextDouble( ) * Constants.PopSize );
-               koTemp = this.voWorkers[ kiW1 ].VoItem1.VoPopulation[ kiM1 ];
-               this.voWorkers[ kiW1 ].VoItem1.VoPopulation[ kiM1 ] = this.voWorkers[ kiW2 ].VoItem1.VoPopulation[ kiM2 ];
-               this.voWorkers[ kiW2 ].VoItem1.VoPopulation[ kiM2 ] = koTemp;
+               kiM1 = ( int )( voRand.NextDouble( ) * Constants.PopSize );
+               kiM2 = ( int )( voRand.NextDouble( ) * Constants.PopSize );
+               koTemp = this.voEcosystem[ kiW1 ].VoWorker.VoPopulation[ kiM1 ];
+               this.voEcosystem[ kiW1 ].VoWorker.VoPopulation[ kiM1 ] = this.voEcosystem[ kiW2 ].VoWorker.VoPopulation[ kiM2 ];
+               this.voEcosystem[ kiW2 ].VoWorker.VoPopulation[ kiM2 ] = koTemp;
+            }
+         }
+      }
+
+      private void mExchangeBBO( )
+      {
+         double kdTotal = 0.0;
+         Member koTemp;
+         int    kiMi, kiMj;
+
+         /// -# Calcualte Lambda and Mu
+         for( int i = 0; i < this.viNumWorkers; i++ )
+         {
+            kdTotal += this.voEcosystem[ i ].VoWorker.VoBest.VdFitness;
+         }
+
+         for( int i = 0; i < this.viNumWorkers; i++ )
+         {
+            this.voEcosystem[ i ].VdMu = this.voEcosystem[ i ].VoWorker.VoBest.VdFitness / kdTotal;
+            this.voEcosystem[ i ].VdLambda = 1.0 - this.voEcosystem[ i ].VdMu;
+         }
+
+         /// -# Select Hi with probability proportional to Lambda
+         for( int i = 0; i < this.viNumWorkers; i++ )
+         {
+            /// -# If Immigrating
+            if( voRand.NextDouble( ) > this.voEcosystem[ i ].VdLambda )
+            {
+               /// -# Select Hj with probability proportional to Mu
+               for( int j = 0; j < this.viNumWorkers; j++ )
+               {
+                  if( ( i != j ) && ( voRand.NextDouble( ) > this.voEcosystem[ i ].VdMu ) )
+                  {
+                     kiMi = ( int )( voRand.NextDouble( ) * Constants.PopSize );
+                     kiMj = ( int )( voRand.NextDouble( ) * Constants.PopSize );
+
+                     /// -# Randomly select an SIV sigma from Hj
+                     koTemp = this.voEcosystem[ j ].VoWorker.VoPopulation[ kiMj ];
+
+                     /// -# Replace a random SIV in Hi with sigma
+                     this.voEcosystem[ j ].VoWorker.VoPopulation[ kiMj ] = this.voEcosystem[ i ].VoWorker.VoPopulation[ kiMi ];
+                     this.voEcosystem[ i ].VoWorker.VoPopulation[ kiMi ] = koTemp;
+                  }
+               }
             }
          }
       }
---
+++ Assignment08/GAWebHost/Habitat.cs
@@ -0,0 +1,20 @@
+﻿namespace GAWebHost
+{
+   using GAWebLib;
+
+   public class Habitat
+   {
+      public Worker VoWorker{ get; set; }    ///< Populations
+      public double VdLambda{ get; set; }    ///< Immigration probability
+      public double VdMu{ get; set; }        ///< Emigration propability
+      public bool   VbComplete{ get; set; }  ///< Habitat complete, ready for exchange
+
+      public Habitat( Worker aoWorker )
+      {
+         this.VoWorker = aoWorker;
+         this.VdLambda = 0.0 ; // new double[ aoWorker.ViSizePop ];
+         this.VdMu     = 0.0 ; // new double[ aoWorker.ViSizePop ];
+         this.VbComplete = false;
+      }
+   }
+}
